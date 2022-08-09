3295bf6fa79cba3bb2f103a8d39f23db59dfebc8
Juwon <tjwndnjs1998@gmail.com>
2022-08-09 22:26:25 +0900

    Initial commit

---
+++ src/main/java/com/eCommerceService/ECommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eCommerceService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceServiceApplication.class, args);
+	}
+
+}
